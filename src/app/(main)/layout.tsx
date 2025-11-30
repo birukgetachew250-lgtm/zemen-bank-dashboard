34f13402819d41e08191b1749172a25aa32cd422
biruk getachew <birukgetachew251@gmail.com>
2025-11-30 13:20:45 +0000

    it is still the same unfortunatly please investigate first

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,7 +13,7 @@ export default function MainLayout({
     <SidebarProvider>
       <div className="flex min-h-screen bg-background">
         <SidebarNav />
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col min-w-0">
           <Header />
           <main className="flex-1 p-4 md:p-6 lg:p-8 relative">
             <Watermark />
