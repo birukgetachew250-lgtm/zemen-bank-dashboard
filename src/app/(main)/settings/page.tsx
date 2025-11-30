3caf63d3a325a5a7f134d7504ab85ec0dcd69fd3
biruk getachew <birukgetachew251@gmail.com>
2025-11-30 15:13:33 +0000

    Try fixing this error: `Runtime Error: Error: FormMessage is not defined

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -12,6 +12,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Switch } from '@/components/ui/switch';
