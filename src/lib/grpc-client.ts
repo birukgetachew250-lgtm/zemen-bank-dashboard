4b0873f420e112f2f0ac08bcaf944678332a029d
biruk getachew <birukgetachew251@gmail.com>
2025-12-18 21:51:22 +0000

    ⨯ ./src/lib/grpc-client.ts:72:8 Ecmascript file had an error   70 |   71

--- src/lib/grpc-client.ts
+++ src/lib/grpc-client.ts
@@ -1,5 +1,5 @@
 
-'use server';
+'server-only';
 
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
@@ -57,7 +57,6 @@ function loadGrpcClient() {
             grpc.credentials.createInsecure()
         );
         
-        // Correctly assign the message type constructor
         accountDetailRequestType = accountDetailPackage.AccountDetailRequest;
 
         console.log("[gRPC Client] gRPC client and message types created successfully.");
