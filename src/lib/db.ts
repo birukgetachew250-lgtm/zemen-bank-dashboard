36895a89a70588a3302a1d42e6ccfd054e27fc21
biruk getachew <birukgetachew251@gmail.com>
2026-01-09 16:14:28 +0000

    Try fixing this error: `Runtime Error: LibsqlError: URL_PARAM_NOT_SUPPOR

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,23 +1,14 @@
 'server-only';
 
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSQL } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
 
 declare global {
   var db: PrismaClient | undefined;
 }
 
-const libsql = createClient({
-  url: process.env.DASH_PROD_MODULE_DATABASE_URL!,
-});
-
-const adapter = new PrismaLibSQL(libsql);
-
 export const db =
   global.db ||
   new PrismaClient({
-    adapter,
     log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
--- src/lib/system-db.ts
+++ src/lib/system-db.ts
@@ -1,24 +1,15 @@
 'server-only';
 
-import { PrismaClient } from '@prisma/client';
-import { PrismaLibSQL } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
+import { PrismaClient } from '@prisma/client/system';
 
 
 declare global {
   var systemDb: PrismaClient | undefined;
 }
 
-const libsql = createClient({
-  url: process.env.SYSTEM_MODULE_DB_URL!,
-});
-
-const adapter = new PrismaLibSQL(libsql);
-
 export const systemDb =
   global.systemDb ||
   new PrismaClient({
-    adapter,
     log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
