d032bba5b1a0b16fc673f42bb825cd0935a50022
biruk getachew <birukgetachew251@gmail.com>
2026-01-09 17:19:40 +0000

    C:\Users\GE68HX\Documents\firebase\zemen-bank-dashboard\.next\server\chu

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,7 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
+  console.log(`[db.ts] Initializing Prisma Client. DATABASE_URL: ${process.env.DATABASE_URL}`);
   return new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
