c412a26f7d310416a943597530f227cb99c23cec
biruk getachew <birukgetachew250@gmail.com>
2026-01-12 08:42:31 +0000

    there is not change ..

--- src/app/api/onboarding/find-customer/route.ts
+++ src/app/api/onboarding/find-customer/route.ts
@@ -43,6 +43,7 @@ export async function POST(req: Request) {
         const client = getAccountDetailServiceClient();
         const accountDetailPackage = getAccountDetailPackage();
         
+        // This is the plain JS object for the nested AccountDetailRequest
         const accountDetailRequestPayload = {
             branch_code: branch_code,
             customer_id: customer_id
